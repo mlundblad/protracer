b37a991436bebff7ab5fa866f9fb19244a4ebcd4
Marcus Lundblad <ml@update.uu.se>
2011-07-21 14:16:54 +0200

    Added C++ guarding

--- src/Light.h
+++ src/Light.h
@@ -27,6 +27,10 @@
 #ifndef LIGHT_H
 #define LIGHT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "Vector.h"
 
 typedef struct
@@ -55,5 +59,8 @@ typedef struct
 
 #define Light_position( l ) (l.position)
 
+#ifdef __cplusplus
+};
+#endif
 
 #endif /* LIGHT_H */
